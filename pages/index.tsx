89906ae4e77c213e21d921407fd5d144f62fa9f7
PotatoHD404 <31506993+PotatoHD404@users.noreply.github.com>
2022-01-17 00:35:36 +0300

    Added part of cas

--- pages/index.tsx
+++ pages/index.tsx
@@ -61,8 +61,8 @@ const Home: NextPage = () => {
                 >
                     Powered by{' '}
                     <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16}/>
-          </span>
+                        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16}/>
+                    </span>
                 </a>
             </footer>
         </div>
