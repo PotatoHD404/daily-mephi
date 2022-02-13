414215cdb0092430294203734f25d1739879265c
PotatoHD404 <31506993+PotatoHD404@users.noreply.github.com>
2022-02-13 18:29:33 +0300

    Added warning images and pages

--- components/footer.tsx
+++ components/footer.tsx
@@ -14,17 +14,17 @@ import Image from "next/image";
 class Footer extends Component {
     render() {
         return (
-            <footer className="text-center text-white grid grid-cols-12 justify-center">
+            <footer className="text-center grid grid-cols-12 justify-center">
                 <div className="bg-black col-start-2 col-end-12 h-1.5 rounded mb-5"/>
-                <div className="text-left col-start-2 col-end-5 pl-1 text-gray-700 p-4 font-semibold">
+                <div className="text-left col-start-2 col-end-5 pl-1 p-4 font-semibold">
                     <div className="text-2xl">support@daily-mephi.ru</div>
                 </div>
 
-                <div className="text-center col-start-6 col-end-8 text-gray-700 p-4 text-2xl font-semibold">
+                <div className="text-center col-start-6 col-end-8 p-4 text-2xl font-semibold">
                     © {new Date().getFullYear()} daily mephi
                 </div>
                 <div
-                    className="col-start-9 flex-row justify-end gap-1 col-end-12 text-gray-700 p-4 text-2xl font-semibold">
+                    className="col-start-9 flex-row justify-end gap-1 col-end-12 p-4 text-2xl font-semibold">
                     <div className="-mt-0.5">
                         <Link href="https://github.com/MEPhI-Floppas/daily-mephi" passHref>
 
