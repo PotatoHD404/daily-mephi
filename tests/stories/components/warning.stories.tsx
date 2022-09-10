6b0ada738198b2b29e059b40328a8d5acfc48643
PotatoHD404 <31506993+PotatoHD404@users.noreply.github.com>
2022-09-10 22:13:17 +0300

    Fixed daily

--- tests/stories/components/warning.stories.tsx
+++
@@ -1,9 +0,0 @@
-import WarningDialog from "../../../components/warning";
-
-
-export default {
-    title: 'WarningDialog',
-    component: WarningDialog
-}
-
-export const WarningDialogComponent = () => <WarningDialog  handleClose={() => {}} opened/>
