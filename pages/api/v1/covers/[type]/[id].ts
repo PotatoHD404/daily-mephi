ff1e6498bbc6bfb45d5d576e25af7f91e0c943a3
PotatoHD404 <31506993+PotatoHD404@users.noreply.github.com>
2022-09-21 01:08:22 +0300

    Trying the og generator

--- pages/api/v1/covers/[type]/[id].ts
+++ pages/api/v1/covers/[type]/[id].ts
@@ -1,12 +1,12 @@
 import type {NextApiRequest, NextApiResponse} from "next";
-import getTutorSvg from "components/getTutorSvg";
 import sharp from 'sharp';
 import path from 'path'
-import { base64Image } from "helpers/consts";
 
-const imageBuffer = Buffer.from(base64Image, 'base64');
 import {getTutor} from "../../tutors/[id]";
-import {UUID_REGEX} from "../../tutors/[id]/materials";
+import ejs from "ejs";
+import {promises as fs} from "fs";
+// import ejs template from file
+
 
 // export const config = {
 //     runtime: 'experimental-edge',
@@ -17,60 +17,54 @@ path.resolve(process.cwd(), 'fonts', 'fonts.conf')
 path.resolve(process.cwd(), 'fonts', 'Montserrat-Medium.ttf')
 
 
-
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<object>
 ) {
-    let {type, id} = req.query;
-    if (!type || typeof type !== "string" || !id || typeof id !== "string" || !id.replace(".png", "").match(UUID_REGEX)) {
-        res.status(400).json({status: "bad request"});
-        return;
-    }
-    id = id.replace(".png", "");
-    let svg: string;
-    switch (type) {
-        case "tutors":
-            // get tutor from prisma
-            const tutor = await getTutor(id);
-            if(tutor === undefined) {
-                res.status(404).json({status: "not found"});
-                return;
-            }
+    const avatarUrl = "https://logos-world.net/wp-content/uploads/2021/08/Among-Us-Logo.png";
+    // fetch image from url and convert it to buffer
+    const avatarString = await fetch(avatarUrl)
+        .then((res) => res.arrayBuffer())
+        .then((buffer) => Buffer.from(buffer))
+        .then((buffer) => "data:image/png;base64," + buffer.toString('base64'));
+
+    // const fontString = await fs.readFile(path.resolve(process.cwd(), 'fonts', 'Roboto-Medium.ttf')).then
+    // (buffer => buffer.toString('base64'));
+
+    const rendered = await ejs.renderFile(path.resolve(process.cwd(), 'thumbnails', 'tutor.ejs'), {
+        tutor_name: "",
+        mephist_rating: "",
+        daily_rating: "",
+        reviews: "",
+        reviews_count: "",
+        materials: "",
+        materials_count: "",
+        rating: "",
+        rating_value: "Текст",
+        image: avatarString,
+    });
+    // convert to image
+    const image = sharp(Buffer.from(rendered));
+    // set headers
+    res.setHeader('Content-Type', 'image/png');
+    // res.setHeader('Content-Type', 'image/svg+xml');
+    // res.setHeader('Cache-Control', 'public, max-age=31536000, immutable');
+    // send image
+    image.png().pipe(res);
+    // res.end(rendered);
+    // let svg = getTutorSvg({
+    //     tutor_name: "",
+    //     mephist_rating: "",
+    //     daily_rating: "",
+    //     reviews: "",
+    //     reviews_count: "",
+    //     materials: "",
+    //     materials_count: "",
+    //     rating: "",
+    //     rating_value: "",
+    //     image: ""
+    // });
 
 
-            let image = tutor.images[0] ? tutor.images[0] : "";
-            svg = getTutorSvg({
-                tutor_name: "",
-                mephist_rating: "",
-                daily_rating: "",
-                reviews: "",
-                reviews_count: "",
-                materials: "",
-                materials_count: "",
-                rating: "",
-                rating_value: "",
-                image
-            });
-            break;
-        default:
-            res.status(404).json({status: "not found"});
-            return;
-    }
-
-
-    const buffer = Buffer.from(svg);
-
-    const image = sharp(buffer).png();
-
-    // 31536000
-    // let doc = new DOMParser().parseFromString(svg, "text/xml");
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "image/png");
-    res.setHeader(
-        "Cache-Control",
-        "public, immutable, no-transform, s-maxage=86400, max-age=86400"
-    );
-    let resultBuffer = await image.toBuffer();
-    return res.end(resultBuffer);
+    // res.status(200).json({status: rendered});
 }
