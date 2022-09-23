8978d731ecb0ef4d23f0a31344e179ba0096f753
PotatoHD404 <31506993+PotatoHD404@users.noreply.github.com>
2022-09-23 17:58:59 +0300

    Adding thumbnails

--- pages/api/v1/thumbnails/[type]/[id].ts
+++ pages/api/v1/thumbnails/[type]/[id].ts
@@ -57,10 +57,13 @@ export default async function handler(
     res.setHeader('Content-Type', 'image/png');
     id = id.replace(".png", "");
     let rendered: Buffer;
+    sharp.cache(false);
+    // sharp.simd(false);
+    sharp.concurrency(1);
     if (type == "tutors") {
         const tutor = await getTutor(id);
         if (!tutor) {
-            res.status(404).json({status: "not found"});
+            returnNotFound(res);
             return;
         }
         let avatarString: string = "";
@@ -74,9 +77,7 @@ export default async function handler(
         }
         // const fontString = await fs.readFile(path.resolve(process.cwd(), 'fonts', 'Roboto-Medium.ttf')).then
         // (buffer => buffer.toString('base64'));
-        sharp.cache(false);
-        // sharp.simd(false);
-        sharp.concurrency(1);
+
         rendered = await ejs.renderFile(path.resolve(process.cwd(), 'thumbnails', 'tutor.ejs'), {
             tutor_name: tutor.lastName + " " + (tutor.firstName ? tutor.firstName[0] + "." : "") +
                 (tutor.fatherName ? tutor.fatherName[0] + "." : ""),
@@ -89,24 +90,61 @@ export default async function handler(
             quotes: getNoun(tutor.quotesCount, "Цитата", "Цитаты", "Цитат"),
             quotes_count: tutor.quotesCount,
             image: avatarString,
-            font_path: process.env.LOCAL == "true" ?
-                "'cloud-functions/main/fonts/Montserrat-Medium.ttf'" :
-                "'Montserrat-Medium.ttf'",
+            font_path: fontPath,
         }).then((html) => Buffer.from(html));
     } else if (type == "materials") {
+        const material = await prisma.material.findUnique({
+            where: {
+                id: id
+            },
+            select: {
+                semesters: {
+                    select: {
+                        name: true
+                    }
+                },
+                faculties: {
+                    select: {
+                        name: true
+                    }
+                },
+                disciplines: {
+                    select: {
+                        name: true
+                    }
+                },
+                header: true,
+                description: true,
+                user: {
+                    select: {
+                        name: true,
+                        image: true
+                    }
+                }
+            }
+        });
+        if (!material) {
+            returnNotFound(res);
+            return;
+        }
         // const material = await getMaterial(id);
+        let avatarString: string = "";
+        if (material.user?.image) {
+            avatarString = await fetch(material.user.image)
+                .then((res) => res.arrayBuffer())
+                .then((buffer) => Buffer.from(buffer))
+                .then((buffer) => "data:image/png;base64," + buffer.toString('base64'));
+        }
+
+
         rendered = await ejs.renderFile(path.resolve(process.cwd(), 'thumbnails', 'material.ejs'), {
-            material_name: "",
-            tutor_name: "",
-            mephist_rating: "",
-            daily_rating: "",
-            reviews: "",
-            reviews_count: "",
-            materials: "",
-            materials_count: "",
-            rating: "",
-            rating_value: "Текст",
-            image: "",
+            nickname: material.user?.name ?? "",
+            header: material.header,
+            description: material.description,
+            semester: material.semesters[0]?.name ?? "",
+            faculty: material.faculties[0]?.name ?? "",
+            discipline: material.disciplines[0]?.name ?? "",
+            image: avatarString,
             font_path: fontPath,
         }).then((html) => Buffer.from(html));
     } else if (type == "quotes") {
@@ -136,28 +174,51 @@ export default async function handler(
             font_path: fontPath
         }).then((html) => Buffer.from(html));
     } else if (type == "users") {
-
-    }
-        // else if(type == "review") {
-        //     const review = await getReview(id);
-        //     rendered = await ejs.renderFile(path.resolve(process.cwd(), 'thumbnails', 'review.ejs'), {
-        //         material_name: "",
-        //         tutor_name: "",
-        //         mephist_rating: "",
-        //         daily_rating: "",
-        //         reviews: "",
-        //         reviews_count: "",
-        //         materials: "",
-        //         materials_count: "",
-        //         rating: "",
-        //         rating_value: "Текст",
-        //         image: "",
-        //         font_path: process.env.LOCAL == "true" ?
-        //             "'cloud-functions/main/fonts/Montserrat-Medium.ttf'" :
-        //             "'Montserrat-Medium.ttf'",
-        //     }).then((html) => Buffer.from(html));
-    // }
-    else {
+        const user = await prisma.user.findUnique({
+            where: {
+                id: id
+            },
+            select: {
+                name: true,
+                image: true,
+                role: true,
+                rating: true,
+                _count: {
+                    select: {
+                        reviews: true,
+                        materials: true,
+                        quotes: true,
+                    }
+                }
+            }
+        });
+        if (!user) {
+            returnNotFound(res);
+            return;
+        }
+        let avatarString: string = "";
+        if (user.image) {
+            avatarString = await fetch(user.image)
+                .then((res) => res.arrayBuffer())
+                .then((buffer) => Buffer.from(buffer))
+                .then((buffer) => "data:image/png;base64," + buffer.toString('base64'));
+        }
+        rendered = await ejs.renderFile(path.resolve(process.cwd(), 'thumbnails', 'user.ejs'), {
+            reviews: getNoun(user._count.reviews, "Отзыв", "Отзыва", "Отзывов"),
+            reviews_count: user._count.reviews,
+            materials: getNoun(user._count.materials, "Материал", "Материала", "Материалов"),
+            materials_count: user._count.materials,
+            quotes: getNoun(user._count.quotes, "Цитата", "Цитаты", "Цитат"),
+            quotes_count: user._count.quotes,
+            nickname: user.name,
+            user_type: user.role == "tutor" ? "Преподаватель" :"Студент",
+            rating: user.rating,
+            image: avatarString,
+            font_path: fontPath,
+        }).then((html) => Buffer.from(html));
+    } else if (type == "reviews") {
+        rendered = new Buffer(0);
+    } else {
         returnNotFound(res);
         return;
     }
@@ -167,4 +228,40 @@ export default async function handler(
     image.cork();
     image.pipe(res);
     image.uncork();
+
+    //   const alphabet: string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZАБВГДЕЁЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзиклмнопрстуфхцчшщъыьэюя0123456789.,!?:;()[]{}@#$%^*-_+=|/\\`~";
+    //   // map alphabet to object
+    //   const alphabetMap: { [key: string]: number } = {};
+    //   for (let i = 0; i < alphabet.length; i++) {
+    //
+    //       const watermark = new Buffer(`<svg>
+    //   <style type="text/css">
+    //       /* set up font */
+    //       @font-face {
+    //           font-family: Montserrat;
+    //           src: url(${fontPath});
+    //       }
+    //       /* apply font to all text elements */
+    //   </style>
+    //   <text y="50" fill="#000" font-family="Montserrat" font-size="30" font-style="italic" font-weight="500" text-anchor="start">${alphabet[i]}</text>
+    // </svg>`)
+    //       const text = sharp(watermark).png();
+    //       let result: { data: any, info: any } | null = null;
+    //       console.log(alphabet[i]);
+    //       while (!result) {
+    //           try {
+    //               result = await text.toBuffer({resolveWithObject: true});
+    //           } catch (e) {
+    //               console.log(e)
+    //           }
+    //       }
+    //       alphabetMap[alphabet[i]] = result.info.width;
+    //   }
+    //   // res header json
+    //   res.setHeader('Content-Type', 'application/json');
+    //
+    //   res.status(200).json(alphabetMap);
+
+    // console.log(result.info.width)
+    // res.end(result.data);
 }
