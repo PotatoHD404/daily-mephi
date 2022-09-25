aa9adc98faa9193412d4c996605ca2a8349b9fa1
PotatoHD404 <31506993+PotatoHD404@users.noreply.github.com>
2022-09-25 20:11:49 +0300

    intermediate commit

--- next.config.js
+++ next.config.js
@@ -19,78 +19,91 @@ const ContentSecurityPolicy = `
 `;
 
 const securityHeaders = [
-  {
-    key: 'X-DNS-Prefetch-Control',
-    value: 'on'
-  },
-  {
-    key: 'X-XSS-Protection',
-    value: '1; mode=block'
-  },
-  {
-    key: 'X-Frame-Options',
-    value: 'SAMEORIGIN'
-  },
-  {
-    key: 'X-Content-Type-Options',
-    value: 'nosniff'
-  },
-  {
-    key: 'Referrer-Policy',
-    value: 'origin-when-cross-origin'
-  }
+    {
+        key: 'X-DNS-Prefetch-Control',
+        value: 'on'
+    },
+    {
+        key: 'X-XSS-Protection',
+        value: '1; mode=block'
+    },
+    {
+        key: 'X-Frame-Options',
+        value: 'SAMEORIGIN'
+    },
+    {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff'
+    },
+    {
+        key: 'Referrer-Policy',
+        value: 'origin-when-cross-origin'
+    }
 ];
 
-if (process.env.NODE_ENV === 'production') {
-  securityHeaders.push({
-    key: 'Content-Security-Policy',
-    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//     securityHeaders.push({
+//         key: 'Content-Security-Policy',
+//         value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+//     });
+// }
+
+// const sharp = "commonjs sharp";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withPWA({
-    // target: 'serverless',
-    swcMinify: true,
-    reactStrictMode: true,
-    webpack: (config) => {
-        config.experiments = {layers: true, topLevelAwait: true};
-        return config;
-    },
-    experimental: {
-        esmExternals: false
-    },
-    async headers() {
-        return [
-          {
-            // Apply these headers to all routes in your application.
-            source: '/:path*',
-            headers: securityHeaders
-          }
-        ];
-      },
-    // module: {
-    //     rules: [
-    //         {
-    //             test: /\.*$/,
-    //             exclude: [
-    //                 './parsing/'
-    //             ]
-    //         }
-    //     ],
-    // },
-    images: {
-        unoptimized: true,
-        domains: ['mc.yandex.ru']
-    },
-    typescript: {
-        // !! WARN !!
-        // Dangerously allow production builds to successfully complete even if
-        // your project has type errors.
-        // !! WARN !!
-        ignoreBuildErrors: false
-    }
-});
+
+
+const nextConfig = withPWA(
+    {
+        target: 'serverless',
+        swcMinify: true,
+        reactStrictMode: true,
+        webpack: (config) => {
+            config.experiments = {layers: true, topLevelAwait: true};
+            // add sharp to externals
+
+            config.module.rules.push({
+                test: /\.node$/,
+                loader: "node-loader"
+            })
+            // config.externals = {...config.externals, sharp};
+
+            return config;
+        },
+        experimental: {
+            esmExternals: false
+        },
+        async headers() {
+            return [
+                {
+                    // Apply these headers to all routes in your application.
+                    source: '/:path*',
+                    headers: securityHeaders
+                }
+            ];
+        },
+        // module: {
+        //     rules: [
+        //         {
+        //             test: /\.*$/,
+        //             exclude: [
+        //                 './parsing/'
+        //             ]
+        //         }
+        //     ],
+        // },
+        images: {
+            unoptimized: true,
+            domains: ['mc.yandex.ru']
+        },
+        typescript: {
+            // !! WARN !!
+            // Dangerously allow production builds to successfully complete even if
+            // your project has type errors.
+            // !! WARN !!
+            ignoreBuildErrors: false
+        }
+    });
 //https://www.npmjs.com/package/next-pwa
 
 module.exports = withPreact(nextConfig);
