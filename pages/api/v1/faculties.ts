00118d77a3b4f98068a89d3af59005d8141b29ff
Artemii <artem-lukichev@bk.ru>
2022-11-04 17:21:10 +0300

    Rewriting to PostgreSQL

--- pages/api/v1/faculties.ts
+++ pages/api/v1/faculties.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type {NextApiRequest, NextApiResponse} from 'next'
 import prisma from "lib/database/prisma";
+import {getClient} from "../../../lib/database/pg";
 
 
 export default async function handler(
@@ -11,7 +12,11 @@ export default async function handler(
         res.status(405).json({status: "method not allowed"});
         return;
     }
-    const faculties = await prisma.faculty.findMany()
+    // const faculties = await prisma.faculty.findMany()
+    const client = await getClient();
+    const {rows: faculties} = await client.query(`
+    SELECT * FROM faculties
+    `)
 
     res.status(200).json(faculties);
 }
