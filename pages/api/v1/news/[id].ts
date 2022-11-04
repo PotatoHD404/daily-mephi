f677c68e6e95198e1a9d455b9676e8e2c4adb8aa
Artemii <artem-lukichev@bk.ru>
2022-11-04 19:28:26 +0300

    More PostgreSQL

--- pages/api/v1/news/[id].ts
+++ pages/api/v1/news/[id].ts
@@ -1,6 +1,7 @@
 import type {NextApiRequest, NextApiResponse} from 'next'
 import prisma from "lib/database/prisma";
 import {UUID_REGEX} from "lib/constants/uuidRegex";
+import {getClient} from "../../../../lib/database/pg";
 
 
 export default async function handler(
@@ -12,20 +13,34 @@ export default async function handler(
         res.status(400).json({status: "bad request"});
         return;
     }
-    const news = prisma.news.findUnique({
-        where: {
-            id
-        },
-        select: {
-            title: true,
-            text: true,
-            createdAt: true,
-            // commentCount: true
-        }
-    });
+    // const news = prisma.news.findUnique({
+    //     where: {
+    //         id
+    //     },
+    //     select: {
+    //         title: true,
+    //         text: true,
+    //         createdAt: true,
+    //         // commentCount: true
+    //     }
+    // });
+    const client = await getClient();
+    const {rows: news} = await client.query(`
+    SELECT news.title,
+             news.text,
+             news.created_at,
+        (SELECT COUNT(*)::INT
+                    FROM reactions
+                    WHERE reactions.news_id = news.id
+                        AND reactions.liked = true)                                                 AS likes,
+        (SELECT COUNT(*)::INT
+                    FROM reactions
+                    WHERE reactions.news_id = news.id
+                        AND reactions.liked = false)                                                AS dislikes,
+        (SELECT COUNT(*)::INT FROM comments WHERE comments.news_id = news.id)                      AS comment_count
+    FROM news
+    WHERE news.id = $1
+    `, [id])
 
-
-    res.status(200).json({
-        news
-    });
+    res.status(200).json({news});
 }
