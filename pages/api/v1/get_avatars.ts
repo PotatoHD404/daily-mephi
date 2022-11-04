74c795560dd3ad262200c09d418dae826dcf5750
Artemii <artem-lukichev@bk.ru>
2022-11-04 19:42:53 +0300

    Avatar

--- pages/api/v1/get_avatars.ts
+++ pages/api/v1/get_avatars.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type {NextApiRequest, NextApiResponse} from 'next'
 import prisma from "lib/database/prisma";
+import {getClient} from "../../../lib/database/pg";
 
 
 export default async function handler(
@@ -11,18 +12,28 @@ export default async function handler(
         res.status(405).json({status: "method not allowed"});
         return;
     }
-    const avatars = await prisma.file.findMany(
-        {
-            where: {
-                tag: "avatar",
-                user: null
-            },
-            select: {
-                url: true,
-                altUrl: true,
-            }
-        }
-    )
+    // const avatars = await prisma.file.findMany(
+    //     {
+    //         where: {
+    //             tag: "avatar",
+    //             user: null
+    //         },
+    //         select: {
+    //             url: true,
+    //             altUrl: true,
+    //         }
+    //     }
+    // )
+    const client = await getClient();
+    const {rows: avatars} = await client.query(`
+    SELECT id,
+           url,
+           alt_url
+    FROM files
+    WHERE tag = 'avatar'
+        AND user_id IS NULL
+    `)
+
 
     res.status(200).json(avatars);
 }
