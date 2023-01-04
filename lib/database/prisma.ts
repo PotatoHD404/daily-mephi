ac09e5d632c479f68441b055b834257c1ee9b418
PotatoHD404 <31506993+PotatoHD404@users.noreply.github.com>
2023-01-04 19:56:38 +0300

    Seems like added prisma entirely

--- lib/database/prisma.ts
+++ lib/database/prisma.ts
@@ -2,6 +2,29 @@ import {PrismaClient} from '@prisma/client';
 
 let prisma: PrismaClient = new PrismaClient();
 
+prisma.$use(async (params, next) => {
+    // Check incoming query type
+    // check if params.model not starts with underscore
+    if (!params.model?.startsWith('_')) {
+        if (params.action == 'delete') {
+            // Delete queries
+            // Change action to an update
+            params.action = 'update'
+            params.args['data'] = {deletedAt: new Date()}
+        }
+        if (params.action == 'deleteMany') {
+            // Delete many queries
+            params.action = 'updateMany'
+            if (params.args.data != undefined) {
+                params.args.data['deletedAt'] = new Date()
+            } else {
+                params.args['data'] = {deletedAt: new Date()}
+            }
+        }
+    }
+    return next(params)
+})
+
 // if (process.env.NODE_ENV === 'production') {
 //     prisma = new PrismaClient();
 // } else {
