a008459055da28a4693ac597e0d6aa49a6b8b5ad
PotatoHD404 <31506993+PotatoHD404@users.noreply.github.com>
2024-01-18 23:17:02 +0300

    Fixed search

--- server/routers/search.ts
+++ server/routers/search.ts
@@ -2,11 +2,140 @@ import {t} from "server/utils";
 import {z} from "zod";
 import {convertGoogleQueryToTsQuery, prepareText} from "lib/database/fullTextSearch";
 import {TRPCError} from "@trpc/server";
-import {User} from "next-auth";
-import {Material, News, Review} from ".prisma/client";
-import {Quote} from "@prisma/client";
+import {DefaultArgs} from "@prisma/client/runtime/library";
+import {Prisma, PrismaClient} from "@prisma/client";
 
-type DocsKeyTypes = "tutor" | "user" | "material" | "review" | "quote" | "news";
+export type DocsKeyTypes = "tutor" | "user" | "material" | "review" | "quote" | "news";
+export type PrismaType = PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>
+const findTutors = async (prisma: PrismaType, ids: string[]) =>
+    prisma.tutor.findMany({
+        where: {
+            id: {in: ids},
+        },
+        select: {
+            id: true,
+            fullName: true,
+            shortName: true,
+            materialsCount: true,
+            reviewsCount: true,
+            quotesCount: true,
+            ratesCount: true,
+            updatedAt: true,
+            images: {
+                select: {
+                    url: true,
+                }
+            },
+        }
+    });
+const findUsers = async (prisma: PrismaType, ids: string[]) =>
+    prisma.user.findMany({
+        where: {
+            id: {in: ids},
+        },
+        select: {
+            id: true,
+            nickname: true,
+            image: {
+                select: {
+                    url: true,
+                }
+            },
+        }
+    });
+const findMaterials = async (prisma: PrismaType, ids: string[]) =>
+    prisma.material.findMany({
+        where: {
+            id: {in: ids},
+        },
+        select: {
+            id: true,
+            title: true,
+            text: true,
+            updatedAt: true,
+            commentsCount: true,
+            likesCount: true,
+            dislikesCount: true,
+            files: {
+                select: {
+                    url: true,
+                }
+            },
+        }
+    });
+
+const findReviews = async (prisma: PrismaType, ids: string[]) =>
+    prisma.review.findMany({
+        where: {
+            id: {in: ids},
+        },
+        select: {
+            id: true,
+            text: true,
+            updatedAt: true,
+            likesCount: true,
+            dislikesCount: true,
+            user: {
+                select: {
+                    id: true,
+                    nickname: true,
+                    image: {
+                        select: {
+                            url: true,
+                        }
+                    },
+                }
+            },
+        }
+    });
+const findQuote = async (prisma: PrismaType, ids: string[]) =>
+    prisma.quote.findMany({
+        where: {
+            id: {in: ids},
+        },
+        select: {
+            id: true,
+            text: true,
+            updatedAt: true,
+            likesCount: true,
+            dislikesCount: true,
+            user: {
+                select: {
+                    id: true,
+                    nickname: true,
+                    image: {
+                        select: {
+                            url: true,
+                        }
+                    },
+                }
+            },
+        }
+    });
+
+const findNews = async (prisma: PrismaType, ids: string[]) =>
+    prisma.news.findMany({
+        where: {
+            id: {in: ids},
+        },
+        select: {
+            id: true,
+            title: true,
+            text: true,
+            updatedAt: true,
+            likesCount: true,
+            dislikesCount: true,
+            commentsCount: true,
+        }
+    });
+
+type Tutor = Awaited<ReturnType<(typeof findTutors)>>[0];
+type User = Awaited<ReturnType<(typeof findUsers)>>[0];
+type Material = Awaited<ReturnType<(typeof findMaterials)>>[0];
+type Review = Awaited<ReturnType<(typeof findReviews)>>[0];
+type Quote = Awaited<ReturnType<(typeof findQuote)>>[0];
+type News = Awaited<ReturnType<(typeof findNews)>>[0];
+
 export const searchRouter = t.router({
     search: t.procedure
         // .meta({
@@ -49,26 +178,6 @@ export const searchRouter = t.router({
                 });
             }
             const tsQuery = convertGoogleQueryToTsQuery(query);
-            // model Document {
-            //     id        String     @id @default(dbgenerated("gen_random_uuid()")) @map("id") @db.Uuid
-            //     text      String     @map("text")
-            //     recordId  String     @unique @db.Uuid
-            //     type      String     @default("unknown") @map("type")
-            //     createdAt DateTime   @default(now()) @map("created_at")
-            //     updatedAt DateTime   @updatedAt @map("updated_at")
-            //     deletedAt DateTime?  @map("deleted_at")
-            //         score     Float      @default(0) @map("score")
-            //     Review    Review[]
-            //     Tutor     Tutor[]
-            //     User      User[]
-            //     Material  Material[]
-            //     News      News[]
-            //     Quote     Quote[]
-            //
-            // @@index([type])
-            // @@index([recordId])
-            // @@map("documents")
-            // }
             interface DocsType {
                 id: string;
                 text: string;
@@ -83,7 +192,7 @@ export const searchRouter = t.router({
             const docs: DocsType[] = await prisma.$queryRaw`
                 SELECT *, similarity("Document"."text", ${tsQuery}) as similarity FROM "Document"
                 WHERE "Document"."text" @@ to_tsquery(${tsQuery})
-                ORDER BY similarity DESC
+                ORDER BY ${}similarity DESC
                 LIMIT ${limit} OFFSET ${offset}`;
 
 
@@ -100,52 +209,6 @@ export const searchRouter = t.router({
             docs.forEach(el => {
                 groupedDocs[el.type].push(el);
             });
-
-            // const prismaMapping = {
-            //     "tutor": prisma["tutor"],
-            //     "user": prisma.user,
-            //     "material": prisma.material,
-            //     "review": prisma.review,
-            //     "quote": prisma.quote,
-            //     "news": prisma.news
-            // }
-
-
-            // export type $TutorPayload<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = {
-            //     name: "Tutor"
-            //     objects: {
-            //         images: Prisma.$FilePayload<ExtArgs>[]
-            //         rates: Prisma.$RatePayload<ExtArgs>[]
-            //         reviews: Prisma.$ReviewPayload<ExtArgs>[]
-            //         disciplines: Prisma.$DisciplinePayload<ExtArgs>[]
-            //         faculties: Prisma.$FacultyPayload<ExtArgs>[]
-            //         materials: Prisma.$MaterialPayload<ExtArgs>[]
-            //         legacyRating: Prisma.$LegacyRatingPayload<ExtArgs> | null
-            //         rating: Prisma.$RatingPayload<ExtArgs> | null
-            //         quotes: Prisma.$QuotePayload<ExtArgs>[]
-            //         document: Prisma.$DocumentPayload<ExtArgs> | null
-            //     }
-            //     scalars: $Extensions.GetPayloadResult<{
-            //         id: string
-            //         firstName: string | null
-            //         lastName: string | null
-            //         fatherName: string | null
-            //         fullName: string
-            //         shortName: string
-            //         nickname: string | null
-            //         url: string | null
-            //         createdAt: Date
-            //         updatedAt: Date
-            //         deletedAt: Date | null
-            //         reviewsCount: number
-            //         materialsCount: number
-            //         quotesCount: number
-            //         ratesCount: number
-            //         score: number
-            //         documentId: string | null
-            //     }, ExtArgs["result"]["tutor"]>
-            //     composites: {}
-            // }
 
             const result: {
                 tutor: Tutor[],
@@ -169,89 +232,30 @@ export const searchRouter = t.router({
                 switch (key) {
                     case "tutor":
                         // we need to select all fields but also the image
-                        const r =  await prisma.tutor.findMany({
-                            where: {
-                                id: {in: ids},
-                            },
-                            select: {
-                                id: true,
-                                firstName: true,
-                                images: {
-                                    select: {
-                                        url: true,
-
-                                    }
-                                },
-                            }
-                        });
+                        result.tutor = await findTutors(prisma, ids);
                         break;
                     case "user":
-                        result.user = await prisma.user.findMany({
-                            where: { id: { in: ids } }
-                        });
+                        result.user = await findUsers(prisma, ids);
                         break;
                     case "material":
-                        result.material = await prisma.material.findMany({
-                            where: { id: { in: ids } }
-                        });
+                        result.material = await findMaterials(prisma, ids);
                         break;
                     case "review":
-                        result.review = await prisma.review.findMany({
-                            where: { id: { in: ids } }
-                        });
+                        result.review = await findReviews(prisma, ids);
                         break;
                     case "quote":
-                        result.quote = await prisma.quote.findMany({
-                            where: { id: { in: ids } }
-                        });
+                        result.quote = await findQuote(prisma, ids);
                         break;
                     case "news":
-                        result.news = await prisma.news.findMany({
-                            where: { id: { in: ids } }
-                        });
+                        result.news = await findNews(prisma, ids);
                         break;
                     default:
                         throw new Error(`Unsupported type: ${key}`);
                 }
             }));
 
-
-
-            //
-            // const docs = await prisma.$queryRaw`
-            //     SELECT * FROM "Document"
-            //     WHERE "Document"."text" @@ to_tsquery(${preparedQuery})`;
-            //
-            //
-            // // console.log(grams);
-            // query = preparedQuery;
-            // const mustBe = Array.from(query.matchAll(/"([^ ]+)"/g), (el) => el[1]);
-            //
-            // const mustNotBe = Array.from(query.matchAll(/-([^ ]+)/g), (el) => el[1]);
-            // // console.log(query, grams, mustBe, mustNotBe);
-            // // const searchResult = await prisma.$queryRaw`
-            // //     SELECT * FROM "Document"`;
-            // let orderBy: Sql = Prisma.empty;
-            // if (sort === "time") {
-            //     orderBy = Prisma.sql`"Document"."timeScore" DESC`;
-            // } else if (sort === "relevance") {
-            //     orderBy = Prisma.sql`"Document"."score" DESC`;
-            // }
-            //
-
-            //
-            //
-            //
-            // return (await Promise.all(Object.entries(results).map(async ([key, value]) => {
-            //     let ids = value.map((el: any) => el.docId);
-            //     const table = prismaMapping[key] as Prisma.TutorDelegate<DefaultArgs>;
-            //     return await table.findMany({
-            //         where: {
-            //             id: {
-            //                 in: ids
-            //             }
-            //         }
-            //     });
-            // }))).flat();
+            return result;
         }),
 });
+
+// export Tutor, User, Material, Review, Quote, News types
